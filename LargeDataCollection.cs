91dfb2b095e52bf0e4ed1bc7c71acab6413b15e8
2901shivam <shivamsingh201617@gmail.com>
2023-11-16 15:25:22 +0530

    Add project files.

---
+++ LargeDataCollection.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1Day9
+{
+    public class LargeDataCollection:IDisposable
+    {
+        private List<Object> dataCollection;
+
+        public LargeDataCollection(List<object>initialdata)
+        {
+            dataCollection = new List<object>(initialdata);
+        }
+
+        public void Add(object item)
+        {
+            dataCollection.Add(item);
+        }
+        public void Remove(object item)
+        {
+            dataCollection.Remove(item);
+        }
+        public object getElement(int index)
+        {
+            try
+            {
+                if (index > 0 && index < dataCollection.Count)
+                {
+                    return dataCollection[index];
+                }
+                else
+                {
+                    return "index out of bound";
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+
+           
+
+        }
+        public void print()
+        {
+            foreach(var item in dataCollection)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+              
+                dataCollection.Clear();
+                dataCollection = null;
+            }
+
+
+        }
+
+        ~LargeDataCollection()
+        {
+            Dispose(false);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1Day9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LargeDataCollection Largedata = new LargeDataCollection(new List<object>() { 1, "Shiv", true, 12.4 });
+            Console.WriteLine("Data at index :"+Largedata.getElement(-1));
+            Largedata.Add(5);
+            Largedata.Remove(1);
+            Largedata.print();
+
+            Largedata.Dispose();
+        }
+    }
+}
